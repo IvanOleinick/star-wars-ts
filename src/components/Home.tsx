fd653834c63b91f16309c87c6568842c405a1aac
ivanOleinick <oleinick.eminemkod31317@gmail.com>
2025-07-30 23:56:53 +0300

    Usa error page with uncorrect iserid

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,24 +1,23 @@
 import Hero from "./Hero.tsx";
 import DreamTeam from "./DreamTeam.tsx";
 import OpeningCrawl from "./OpeningCrawl.tsx";
-import {useParams} from "react-router";
-import {useContext, useEffect} from "react";
-import {StarWarsContext} from "../utils/context.ts";
-import {characters, defaultHero} from "../utils/constants.ts";
+
 import ErrorPage from "./ErrorPage.tsx";
+import {useErrorPage} from "./hooks/useErrorPage.tsx";
 
 
 const Home = () => {
-    const {heroId} = useParams();
-    const {changeHero} = useContext(StarWarsContext);
-    const key = heroId ??  defaultHero
+    const {isError}=useErrorPage();
+    // const {heroId} = useParams();
+    // const {changeHero} = useContext(StarWarsContext);
+    // const key = heroId ??  defaultHero
+    //
+    // useEffect(() => {
+    //     if(!(key in characters)) return;
+    //     changeHero(key)
+    // }, [key])
 
-    useEffect(() => {
-        if(!(key in characters)) return;
-        changeHero(key)
-    }, [key])
-
-    return (key in characters) ? (
+    return !isError ? (
         <main className="clear-both">
             <Hero/>
             <DreamTeam/>
---
+++ src/components/hooks/useErrorPage.tsx
@@ -0,0 +1,19 @@
+import {useParams} from "react-router";
+import {useContext, useEffect} from "react";
+import {StarWarsContext} from "../../utils/context.ts";
+import {characters, defaultHero} from "../../utils/constants.ts";
+
+export const useErrorPage = () => {
+    const {heroId} = useParams();
+    const {changeHero} = useContext(StarWarsContext);
+    const key = heroId ?? defaultHero
+
+    useEffect(() => {
+        if (!(key in characters)) return;
+        changeHero(key)
+    }, [key])
+    return {
+        isError: !(key in characters),
+        key
+    }
+}
